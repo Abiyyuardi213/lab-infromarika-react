ba2d3e32ebb636728d29d00a46e8321f2d5025f7
Ismaindra <imismaindra@gmail.com>
2025-01-30 19:55:24 +0700

    nambah dropdown menu di sidebar dashboard

--- resources/js/Components/Dashboard/Sidebar.jsx
+++ resources/js/Components/Dashboard/Sidebar.jsx
@@ -6,6 +6,9 @@ import {
     ListItem,
     ListItemPrefix,
     Button,
+    Accordion,
+    AccordionHeader,
+    AccordionBody,
 } from "@material-tailwind/react";
 import {
     PresentationChartBarIcon,
@@ -13,9 +16,15 @@ import {
     UserPlusIcon,
     ShoppingCartIcon,
 } from "@heroicons/react/24/solid";
+import { ChevronRightIcon, ChevronDownIcon } from "@heroicons/react/24/outline";
 import { Link } from "@inertiajs/react";
 
 export default function Sidebar({ show }) {
+    const [open, setOpen] = React.useState(0);
+
+    const handleOpen = (value) => {
+        setOpen(open === value ? 0 : value);
+    };
     return (
         <aside
             className={`fixed top-0 left-0 z-40 h-screen transition-transform
@@ -39,17 +48,65 @@ export default function Sidebar({ show }) {
                             Dashboard
                         </Link>
                     </ListItem>
-                    <ListItem className="hover:bg-blue-50">
-                        <ListItemPrefix>
-                            <PresentationChartBarIcon className="h-5 w-5 text-blue-600" />
-                        </ListItemPrefix>
-                        <Link
-                            href="/praktikum"
-                            className="w-full text-gray-700"
-                        >
-                            Praktikum
-                        </Link>
-                    </ListItem>
+                    <Accordion
+                        open={open === 1}
+                        icon={
+                            <ChevronDownIcon
+                                strokeWidth={2.5}
+                                className={`mx-auto h-4 w-4 transition-transform ${
+                                    open === 1 ? "rotate-180" : ""
+                                }`}
+                            />
+                        }
+                    >
+                        <ListItem className="p-0" selected={open === 1}>
+                            <AccordionHeader
+                                onClick={() => handleOpen(1)}
+                                className="border-b-0 p-3"
+                            >
+                                <ListItemPrefix>
+                                    <PresentationChartBarIcon className="h-5 w-5" />
+                                </ListItemPrefix>
+                                <Typography
+                                    color="blue-gray"
+                                    className="mr-auto font-normal"
+                                >
+                                    Praktikum
+                                </Typography>
+                            </AccordionHeader>
+                        </ListItem>
+                        <AccordionBody className="py-1">
+                            <List className="p-0">
+                                <ListItem>
+                                    <ListItemPrefix>
+                                        <ChevronRightIcon
+                                            strokeWidth={3}
+                                            className="h-3 w-5"
+                                        />
+                                    </ListItemPrefix>
+                                    Analytics
+                                </ListItem>
+                                <ListItem>
+                                    <ListItemPrefix>
+                                        <ChevronRightIcon
+                                            strokeWidth={3}
+                                            className="h-3 w-5"
+                                        />
+                                    </ListItemPrefix>
+                                    Reporting
+                                </ListItem>
+                                <ListItem>
+                                    <ListItemPrefix>
+                                        <ChevronRightIcon
+                                            strokeWidth={3}
+                                            className="h-3 w-5"
+                                        />
+                                    </ListItemPrefix>
+                                    Projects
+                                </ListItem>
+                            </List>
+                        </AccordionBody>
+                    </Accordion>
                     <ListItem className="hover:bg-blue-50">
                         <ListItemPrefix>
                             <PresentationChartBarIcon className="h-5 w-5 text-blue-600" />
